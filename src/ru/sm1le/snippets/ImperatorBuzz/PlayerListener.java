c5e632cd9fbe398d40b9ff7c6ed23d685e53c3be
MrSm1le <37928904+MrSm1le@users.noreply.github.com>
2018-05-07 19:50:11 +0400

    Update and rename PlayerListener to PlayerListener.java

---
+++ src/ru/sm1le/snippets/ImperatorBuzz/PlayerListener.java
@@ -0,0 +1,15 @@
+package ru.sm1le.snippets.ImperatorBuzz;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class PlayerListener implements Listener {
+
+	@EventHandler
+	public void onMove(PlayerMoveEvent e) {
+		if(e.getFrom().equals(e.getTo())) {
+			//TODO
+		}
+	}
+}
