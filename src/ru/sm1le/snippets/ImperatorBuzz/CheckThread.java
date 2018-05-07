6fe437b307acf3d1ce08bf04cace483faa99cb57
MrSm1le <37928904+MrSm1le@users.noreply.github.com>
2018-05-07 20:41:54 +0400

    Update CheckThread.java

--- src/ru/sm1le/snippets/ImperatorBuzz/CheckThread.java
+++ src/ru/sm1le/snippets/ImperatorBuzz/CheckThread.java
@@ -3,6 +3,7 @@ package ru.sm1le.snippets.ImperatorBuzz;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -10,10 +11,12 @@ public class CheckThread extends Thread {
 	
 	private volatile boolean isRunning = false;
 	private Material checkMat;
+	private Plugin plugin;
 
-	public CheckThread(Material checkMat) {
+	public CheckThread(Material checkMat, Plugin plugin) {
 		super();
 		this.checkMat = checkMat;
+		this.plugin = plugin;
 	}
 	
 	public void start() {
@@ -30,7 +33,14 @@ public class CheckThread extends Thread {
 		while(isRunning) {
 			for(Player current : Bukkit.getOnlinePlayers()) {
 				if(current.getLocation().clone().add(0, -1, 0).getBlock().getType() == checkMat) {
-					current.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 40, 2));
+					Bukkit.getScheduler().runTask(plugin, new Runnable() {
+						
+						@Override
+						public void run() {
+							current.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 40, 2));
+						}
+						
+					});
 				}
 			}
 			try {
