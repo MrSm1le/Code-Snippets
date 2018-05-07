6df26fe1922f303a213e4e46356b50ecc60fabdf
MrSm1le <37928904+MrSm1le@users.noreply.github.com>
2018-05-07 19:57:58 +0400

    Add files via upload

---
+++ src/ru/sm1le/snippets/ImperatorBuzz/CheckThread.java
@@ -0,0 +1,38 @@
+package ru.sm1le.snippets.ImperatorBuzz;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class CheckThread extends Thread {
+	
+	private volatile boolean isRunning = false;
+	private Location checkLocation;
+
+	public CheckThread(Location loc) {
+		super();
+		checkLocation = loc;
+	}
+	
+	public void start() {
+		isRunning = true;
+		this.start();
+	}
+	
+	public void stopThread() {
+		isRunning = false;
+	}
+	
+	@Override
+	public void run() {
+		while(isRunning) {
+			for(Player current : Bukkit.getOnlinePlayers()) {
+				if(current.getLocation().equals(checkLocation)) {
+					current.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 40, 2));
+				}
+			}
+		}
+	}
+}
